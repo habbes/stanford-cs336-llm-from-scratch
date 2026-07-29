3cc561e6123c8fdfcb7434ccdd9fb6c62ba0cce5
Clément Habinshuti <haby_habbes@live.com>
2026-07-29 04:02:50 +0300

    Rename param counter

--- assignment1-basics/cs336_basics/resource_accounting.py
+++ assignment1-basics/cs336_basics/resource_accounting.py
@@ -15,7 +15,7 @@ class HyperParams:
     def d_k(self):
         return self.d_model // self.num_heads
 
-class ParamContainer(ABC):
+class ParamCounter(ABC):
     def __init__(self, name: str):
         self.name = name
 
@@ -26,7 +26,7 @@ class ParamContainer(ABC):
     def get_num_params(self, hyper_params: HyperParams):
         pass
 
-class LeafContainer(ParamContainer):
+class LeafCounter(ParamCounter):
     def __init__(self, name: str, params_fn: Callable[[HyperParams], int]):
         super().__init__(name)
         self.params_fn = params_fn
@@ -34,8 +34,8 @@ class LeafContainer(ParamContainer):
     def get_num_params(self, hyper_params: HyperParams):
         return self.params_fn(hyper_params)
 
-class CompositeContainer(ParamContainer):
-    def __init__(self, name: str, *children: ParamContainer):
+class CompositeCounter(ParamCounter):
+    def __init__(self, name: str, *children: ParamCounter):
         super().__init__(name)
         self.children = children
     
@@ -45,21 +45,21 @@ class CompositeContainer(ParamContainer):
 
 
 def create_params_counter(num_layers: int):
-    params_counter = CompositeContainer("TransformerLM",
-        LeafContainer("Embedding", lambda h: h.vocab_size * h.d_model),
+    params_counter = CompositeCounter("TransformerLM",
+        LeafCounter("Embedding", lambda h: h.vocab_size * h.d_model),
         # TODO: is RoPE worth mentioning, it has no trainable params but holds some buffer
         # LeafContainer("RoPE", lambda h: h.context_length * h.d_k * h.d_k),
-        CompositeContainer("RMSNorm",
-            LeafContainer("g", lambda h: h.d_model),
-        CompositeContainer("TransformerLayers",
-            (CompositeContainer("TransformerBlock",
-                CompositeContainer("RMSNorm",
-                    LeafContainer("g", lambda h: h.d_model),
-                CompositeContainer("MultiHeadSelfAttention",
-                    LeafContainer("Wq", lambda h: h.d_model * h.num_heads * h.d_k)))) for _ in range(num_layers)))),
+        CompositeCounter("RMSNorm",
+            LeafCounter("g", lambda h: h.d_model),
+        CompositeCounter("TransformerLayers",
+            (CompositeCounter("TransformerBlock",
+                CompositeCounter("RMSNorm",
+                    LeafCounter("g", lambda h: h.d_model),
+                CompositeCounter("MultiHeadSelfAttention",
+                    LeafCounter("Wq", lambda h: h.d_model * h.num_heads * h.d_k)))) for _ in range(num_layers)))),
                     # TODO complete MHSA and TransformerBlock
-        CompositeContainer("RMSNorm (output)",
-            LeafContainer("g", lambda h: h.d_model)),
-        LeafContainer("Linear (LM Head)", lambda h: h.d_model * h.vocab_size))
+        CompositeCounter("RMSNorm (output)",
+            LeafCounter("g", lambda h: h.d_model)),
+        LeafCounter("Linear (LM Head)", lambda h: h.d_model * h.vocab_size))
     
     return params_counter
