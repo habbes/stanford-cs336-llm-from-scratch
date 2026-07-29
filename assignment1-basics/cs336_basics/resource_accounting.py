b10b8149d8d2b318288b152889f9be60d48fecbd
Clément Habinshuti <haby_habbes@live.com>
2026-07-29 04:16:13 +0300

    Create transformer params counter

--- assignment1-basics/cs336_basics/resource_accounting.py
+++ assignment1-basics/cs336_basics/resource_accounting.py
@@ -43,23 +43,48 @@ class CompositeCounter(ParamCounter):
         return sum(
             map(lambda child: child.get_num_params(hyper_params), self.children))
 
+class RepeatCounter(ParamCounter):
+    def __init__(self, name: str, element_count: int, element: ParamCounter):
+        super().__init__(name)
+        self.element_count = element_count
+        self.element = element
+    
+    def get_num_params(self, hyper_params: HyperParams):
+        return self.element_count * self.element.get_num_params(hyper_params)
 
-def create_params_counter(num_layers: int):
+
+def create_transformer_params_counter(num_layers: int):
+    """
+    Creates a resource counter that returns the total
+    number of trainable params in a transformer architecture
+    with the specified number of transformer layers given
+    a specific set of hyper parameters.
+    """
     params_counter = CompositeCounter("TransformerLM",
         LeafCounter("Embedding", lambda h: h.vocab_size * h.d_model),
         # TODO: is RoPE worth mentioning, it has no trainable params but holds some buffer
         # LeafContainer("RoPE", lambda h: h.context_length * h.d_k * h.d_k),
         CompositeCounter("RMSNorm",
             LeafCounter("g", lambda h: h.d_model),
-        CompositeCounter("TransformerLayers",
-            (CompositeCounter("TransformerBlock",
+        RepeatCounter("TransformerLayers", num_layers,
+            CompositeCounter("TransformerBlock",
                 CompositeCounter("RMSNorm",
                     LeafCounter("g", lambda h: h.d_model),
                 CompositeCounter("MultiHeadSelfAttention",
-                    LeafCounter("Wq", lambda h: h.d_model * h.num_heads * h.d_k)))) for _ in range(num_layers)))),
-                    # TODO complete MHSA and TransformerBlock
+                    LeafCounter("Wq", lambda h: h.d_model * h.num_heads * h.d_k),
+                    LeafCounter("Wk", lambda h: h.d_model * h.num_heads * h.d_k),
+                    LeafCounter("Wv", lambda h: h.d_model * h.num_heads * h.d_k),
+                    LeafCounter("Wo", lambda h: h.num_heads * h.d_k * h.d_model)),
+                CompositeCounter("RMSNorm",
+                    LeafCounter("g", lambda h: h.d_model)),
+                CompositeCounter("FFSwiGLU",
+                    LeafCounter("W1", lambda h: h.d_model * h.d_ff),
+                    LeafCounter("W3", lambda h: h.d_model * h.d_ff),
+                    LeafCounter("W2", lambda h: h.d_ff * h.d_model)),
+                CompositeCounter("RMSNorm",
+                    LeafCounter("g", lambda h: h.d_model))))),
         CompositeCounter("RMSNorm (output)",
             LeafCounter("g", lambda h: h.d_model)),
-        LeafCounter("Linear (LM Head)", lambda h: h.d_model * h.vocab_size))
+        LeafCounter("Linear (LM Head)", lambda h: h.d_model * h.vocab_size)))
     
     return params_counter
